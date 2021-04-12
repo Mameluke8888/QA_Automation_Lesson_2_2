fdc7f4a3258c8e7f64789aa27542c8ea4ba1828c
Evgeny Abdulin <evgenyabdulin@Evgenys-Mac-mini.local>
2021-04-12 16:32:09 -0500

    lesson_2_2.py added

---
+++ lesson_2_2.py
@@ -0,0 +1,21 @@
+
+my_string = "Test Automation"
+
+#Slice the string in such a way that it will print only "Test".
+
+word_test = my_string[0:4]
+print(word_test)
+
+#Slice the string in such a way that it will print only "Automation".
+second_word = my_string[5:]
+print(second_word)
+
+#Slice the second and third elements (50 and 20) in the list.
+
+my_list = [10,50,20,25,30]
+
+#Type your answer here
+
+sliced_list = my_list[1:3]
+print(sliced_list)
+
